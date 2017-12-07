b7d142e606c7114e413f5bae316b873dc0430b31
Daren Alfred <daren1997@hotmail.co.uk>
2017-12-07 00:30:06 +0000

    first commit

---
+++ app/src/main/java/com/example/daren/myapplication/Logs_tab.java
@@ -0,0 +1,24 @@
+package com.example.daren.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daren on 06/12/2017.
+ */
+
+public class Logs_tab extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.log,container,false);
+        return myView;
+    }
+}
