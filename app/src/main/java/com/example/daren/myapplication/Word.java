4836ff841b2a0ded15cf96f18c30c3cf6a47c814
Nequo <nedxtreme@gmail.com>
2018-02-19 14:48:23 +0000

    Extracted 1 downloader class from DefTab, added classes for words and sessions

---
+++ app/src/main/java/com/example/daren/myapplication/Word.java
@@ -0,0 +1,16 @@
+package com.example.daren.myapplication;
+
+/**
+ * Created by nadim on 19/02/18.
+ */
+
+public class Word {
+    private String word;
+    private String definition;
+
+
+    public Word(String word, String definition) {
+        this.word = word;
+        this.definition = definition;
+    }
+}
